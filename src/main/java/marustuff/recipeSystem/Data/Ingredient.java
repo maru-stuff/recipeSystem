e6c14d2770e6789c6234717199d18012de4c1f7d
maru-stuff <norepy@example.com>
2021-12-19 18:51:48 +0100

    fixes

--- src/main/java/marustuff/recipeSystem/Data/Ingredient.java
+++ src/main/java/marustuff/recipeSystem/Data/Ingredient.java
@@ -1,12 +1,9 @@
 package marustuff.recipeSystem.Data;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
