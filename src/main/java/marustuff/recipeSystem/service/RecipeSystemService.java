5d84dfa1c59ef49d35fb70934f0b30a42750d298
maru-stuff <norepy@example.com>
2021-12-19 19:01:31 +0100

    fixes

--- src/main/java/marustuff/recipeSystem/service/RecipeSystemService.java
+++ src/main/java/marustuff/recipeSystem/service/RecipeSystemService.java
@@ -1,14 +1,13 @@
 package marustuff.recipeSystem.Service;
 
 import lombok.RequiredArgsConstructor;
-import marustuff.recipeSystem.Data.*;
-import marustuff.recipeSystem.Data.ResourceMapper;
+import marustuff.recipeSystem.Data.DatabaseMapper;
+import marustuff.recipeSystem.Data.IngredientWithAmount;
+import marustuff.recipeSystem.Data.Recipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,44 +15,34 @@ public class RecipeSystemService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private final ResourceMapper resourceMapper;
+    private final DatabaseMapper databaseMapper; //no nie :P trochę zbyt wszystkorobiąca nazwa
 
-    @Autowired
-    private final IngredientMapper ingredientMapper;
-
-    @Autowired
-    private final RecipeMapper recipeMapper;
-
-
-
-    public Recipe getRecipeById(Long id) {
-        Recipe recipe = recipeMapper.findRecipeById(id);
-        recipe.setIngredientsWithAmounts(recipeMapper.getIngredientWithAmountListForRecipeId(id));
+    public Recipe getRecipeById(Long id){
+        Recipe recipe = databaseMapper.findRecipeById(id);
+        recipe.setIngredientsWithAmounts(databaseMapper.getIngredientWithAmountListForRecipeId(id));
         logger.info(recipe.toString());
         return recipe;
     }
 
     public boolean saveRecipe(Recipe recipe) {
-        if (recipe != null && recipe.getName() != null && recipe.getInstructions() != null)
-            recipeMapper.insertRecipe(recipe);
+        // dopuszczasz przepisy z samymi nullami :P?
+        databaseMapper.insertRecipe(recipe);
         logger.info(recipe.toString());
         for (IngredientWithAmount ingredient : recipe.getIngredientsWithAmounts()) {
-            boolean isIngredientPresent =ingredientMapper.isIngredientPresentByName(ingredient.getName());
-            logger.info("Status of search" + isIngredientPresent );
-            if (ingredient != null && ingredient.getName() != null && !ingredient.getName().equals("")) {
-                if (!isIngredientPresent) {
-                    ingredientMapper.insertIngredient(ingredient);
+            logger.info("Status of search"+databaseMapper.isIngredientPresentByName(ingredient.getName()));
+            if(ingredient.getName()!=null&&!ingredient.getName().equals("")){
+                if (!databaseMapper.isIngredientPresentByName(ingredient.getName())) {
+                    databaseMapper.insertIngredient(ingredient);
                     logger.info(ingredient.toString());
-                } else {
-                    long ingredientId=ingredientMapper.getIngredientIdByName(ingredient.getName());
-                    logger.info("get id by name" + ingredientId);
-                    ingredient.setId(ingredientId);
-                    logger.info("Reused ingredient" + ingredient);
+                } else{
+                    logger.info("get id by name"+databaseMapper.getIngredientIdByName(ingredient.getName()));
+                    ingredient.setId(databaseMapper.getIngredientIdByName(ingredient.getName()));
+                    logger.info("Reused ingredient"+ingredient.toString());
                 }
 
             }
             try {
-                resourceMapper.insertRecipeIngredientsAmount(ingredient.getId(), recipe.getId(), ingredient.getAmount());
+                databaseMapper.insertRecipeIngredientsAmount(ingredient.getId(), recipe.getId(), ingredient.getAmount());
             } catch (Exception e) {
                 logger.error(e.getMessage());
                 return false;
@@ -64,25 +53,13 @@ public class RecipeSystemService {
         return true;
     }
 
-    public int getNumberOfRecipePages() {
-        double numberOfPages = resourceMapper.getNumberOfPages();
-        if (numberOfPages == (int) numberOfPages) {
+    public int getNumberOfRecipePages(){
+        double numberOfPages =databaseMapper.getNumberOfPages();
+        if(numberOfPages==(int)numberOfPages){
             return (int) numberOfPages;
-        } else {
+        }else {
             return (int) numberOfPages + 1;
         }
-    }
-
-    public List<Recipe> searchForRecipes(SearchEntity searchEntity) {
-        List<Recipe> foundRecipes;
-        if (searchEntity.getSearchByRecipeName() != null && searchEntity.getSearchByRecipeName() != "") {
-            foundRecipes = resourceMapper.searchByRecipeName(searchEntity.getSearchByRecipeName());
-        } else if (searchEntity.getSearchByIngredient() != null && searchEntity.getSearchByIngredient() != "") {
-            foundRecipes = resourceMapper.searchByIngredient(searchEntity.getSearchByIngredient());
-        } else {
-            foundRecipes = null;
-        }
-        return foundRecipes;
     }
 
 
